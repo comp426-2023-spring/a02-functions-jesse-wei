a85800ef622991fd64e4f42c849722ce1ad718ac
Jesse Wei <jessew13@email.unc.edu>
2023-01-25 14:54:04 -0500

    Extra squeaky clean

--- cli.js
+++ cli.js
@@ -8,13 +8,7 @@ var argv = (minimist)(process.argv.slice(2));
 
 // Help message
 if ('h' in argv) {
-    console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE");
-    console.log("    -h            Show this help message and exit.");
-    console.log("    -n, -s        Latitude: N positive; S negative.");
-    console.log("    -e, -w        Longitude: E positive; W negative.");
-    console.log("    -z            Time zone: uses tz.guess() from moment-timezone by default.");
-    console.log("    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.");
-    console.log("    -j            Echo pretty JSON from open-meteo API and exit.");
+    console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE\n    -h            Show this help message and exit.\n    -n, -s        Latitude: N positive; S negative.\n    -e, -w        Longitude: E positive; W negative.\n    -z            Time zone: uses tz.guess() from moment-timezone by default.\n    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.\n    -j            Echo pretty JSON from open-meteo API and exit.\n");
     process.exit(0);
 }
 
